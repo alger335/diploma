0066588ceaefbc7de740061417f7c1ba4b22dbf3
Alexey Germanov <alger277@gmail.com>
2021-04-23 11:52:12 +0300

    Added folder creation, upload function, logging, progres bar

--- photos_backup.py
+++ photos_backup.py
@@ -10,19 +10,25 @@ with open('token_vk.txt', 'r') as file_object:
 with open('token_ya.txt', 'r') as file_object:
     token_ya = file_object.read().strip()
 
-userid = '552934290'
 
-## Задание:
-# Нужно написать программу, которая будет:
-# 1. Получать фотографии с профиля. Для этого нужно использовать метод [photos.get](https://vk.com/dev/photos.get).
-# 2. Сохранять фотографии максимального размера(ширина/высота в пикселях) на Я.Диске.
-# 3. Для имени фотографий использовать количество лайков.
-# 4. Сохранять информацию по фотографиям в json-файл с результатами.
+# 1. Для загруженных фотографий нужно создать свою папку.
+# 2. Сделать прогресс-бар
+# 3. Оформить код в функции и классы
+# 4. Поправить photos_json
+# Добавить функционал:
+# - Сохранять фотографии и из других альбомов.
+# - Сохранять фотографии из других социальных сетей. [Одноклассники](https://apiok.ru/) и [Инстаграмм](https://www.instagram.com/developer/)
+# - Сохранять фотографии на Google.Drive.
+# - сделать удобную менюшку
+# - проверка контрольной суммы
 
-# vk_user_id = str(input("Введите id пользователя: "))
+# userid = '2236706'  # '552934290'
+photo_range = int(input("Введите количество фотографий для загрузки(по умолчанию 5): "))
+
+vk_user_id = str(input("Введите id пользователя: "))
 URL = 'https://api.vk.com/method/photos.get'
 params = {
-    'user_id': userid,
+    'user_id': vk_user_id,
     'album_id': 'profile',
     'extended': 1,
     'photo_sizes': 1,
@@ -30,15 +36,14 @@ params = {
     'v': '5.130',
 }
 
-
-res = requests.get(URL, params=params, verify=False)
+res = requests.get(URL, params=params)  # , verify=False)
 photos = res.json()['response']['items']
 urls = []
 photo_json = {}
 photos_json = []
 name_url = {}
 # обрабатываем каждую фотографию
-for i in range(len(photos)):
+for i in range(photo_range):
     # берём ссылку на максимальный размер фотографии
     photo_url = str(photos[i]['sizes'][len(photos[i]['sizes']) - 1]['url'])
     # считаем лайки
@@ -47,7 +52,7 @@ for i in range(len(photos)):
     photo_size = str(photos[i]['sizes'][len(photos[i]['sizes']) - 1]['type'])
     # берем дату публикации фотки
     ts = (photos[i]['date'])
-    date = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+    date = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S')
     # формируем словарь с именем файла и размером
     file_name = f'{likes_count}-{date}.jpg'
     photo_json['file_name'] = file_name
@@ -59,58 +64,5 @@ for i in range(len(photos)):
     # собираем список ссылок на фотки
     urls.append(photo_url)
 
-pprint(name_url)
-
-
-
 ya = YaUploader(token=token_ya)
 ya.upload_file_to_disk(name_url)
-
-
-
-
-
-
-# class VkUser:
-#     url = 'https://api.vk.com/method/'
-#
-#     def __init__(self, token, version):
-#         self.token = token
-#         self.version = version
-#         self.params = {
-#             'user_id': '552934290',
-#             'access_token': self.token,
-#             'v': self.version
-#         }
-#         self.owner_id = requests.get(self.url + 'users.get', self.params).json()['response'][0]['id']
-#
-#     def get_followers(self, user_id=None):
-#         if user_id is None:
-#             user_id = self.owner_id
-#         followers_url = self.url + 'users.getFollowers'
-#         followers_params = {
-#             'count': 1000,
-#             'user_id': user_id
-#         }
-#         res = requests.get(followers_url, params={**params, **followers_params})
-#         return res.json()
-#
-#     def get_groups(self, user_id=None):
-#         if user_id is None:
-#             user_id = self.owner_id
-#         groups_url = self.url + 'groups.get'
-#         groups_params = {
-#             'count': 1000,
-#             'user_id': user_id,
-#             'extended': 1,
-#             'fields': 'members_count'
-#         }
-#         res = requests.get(groups_url, params={**params, **groups_params})
-#         return res.json()
-#
-#     # In[13]:
-#
-#
-# # получим свои группы
-# vk_client = VkUser(token, '5.130')
-# vk_client.get_groups(552934290)
--- yandex.py
+++ yandex.py
@@ -1,5 +1,8 @@
 import requests
-import os
+from datetime import datetime, date, time
+from pprint import pprint
+import time
+from tqdm import tqdm
 
 
 class YaUploader:
@@ -17,13 +20,50 @@ class YaUploader:
         headers = self.get_headers()
         params = {"path": disk_file_path, "overwrite": "true"}
         response = requests.get(upload_url, headers=headers, params=params)
+        # pprint(response.json())
         return response.json()
 
+    def create_folder(self):
+        mkdir_url = "https://cloud-api.yandex.net/v1/disk/resources"
+        headers = self.get_headers()
+        dirname = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        dirname = f'VK{dirname}'
+        params = {"path": dirname}
+        response = requests.put(mkdir_url, headers=headers, params=params)
+        # print(response.json())
+        return dirname
+
     def upload_file_to_disk(self, name_url):
-        for key, value in name_url:
+        directory = self.create_folder()
+        timestamp = datetime.now().strftime('[%H:%M:%S %d-%m-%Y]:')
+        for key, value in name_url.items():
+            # print(key)
+            # print(value)
             # filename = key
-            href = self._get_upload_link(f"netology/{key}").get("href", "")
-            response = requests.put(href, data=open(filename, 'rb'))
+            # получаем ссылку для загруски на диск
+            href = self._get_upload_link(f"{directory}/{key}").get("href", "")
+            # pprint(href)
+            # считываем файл по ссылке
+            r = requests.get(value)
+            image_data = r.content
+            # загружаем файл на яндекс диск
+            response = requests.put(href, data=image_data)
+            # прогресс-бар
+            mylist = [1, 2, 3, 4, 5]
+            for i in tqdm(mylist):
+                time.sleep(1)
             response.raise_for_status()
             if response.status_code == 201:
-                print(f"Файл {filename} успешно загружен!")
+                message = f"{timestamp} Файл {key} успешно загружен!"
+                print(message)
+                with open('upload_log.txt', 'a') as logfile:
+                    logfile.write(f'{message}\n')
+            time.sleep(1)
+
+
+
+#
+# mylist = [1,2,3,4,5,6,7,8]
+# for i in tqdm(range(len(name_url))):
+# for i in tqdm(mylist):
+#     time.sleep(1)
